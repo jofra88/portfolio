8c7b9522570b7ab00d3aa78c2fac60e50dee50fc
jofra88 <jofra078@gmail.com>
2024-02-14 11:20:40 +0530

    first

---
+++ src/data/constants.js
@@ -0,0 +1,166 @@
+export const Bio = {
+    name: "Joseph Francis",
+    roles: [
+      "Full Stack Developer",
+      "ML Engineer",
+      "Data Analyst",
+      "Programmer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/jofra88",
+    resume:
+      "https://drive.google.com/file/d/19spleRXcMfNLa9m61MnjTklwLQtDky30/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/joseph-francis-50a38526a",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+      ],
+    },
+    {
+      title: "Machine Learning",
+      skills: [
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "Tenserflow",
+          image:
+            "https://static-00.iconduck.com/assets.00/tensorflow-icon-1911x2048-1m2s54vn.png",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://www.tcckerala.com/themes/user/images/logo.png",
+      role: "Engineering Trainee",
+      company: "TCC",
+      date: "May 2023-June 2023",
+      desc: "Gained industrial experience by undergoing In-Plant training in a State Govt company producing chlor-alkali products .Worked in 110KV Sub-Station and learned about High Tension power supply and distribution to various feeder circuits.Motors,Transformers,Rectifiers,CT,PT,Isolators,Circuit Breakers etc were observed ,carefully",
+      skills: [
+        "Electrical Engineering",
+      ],
+      doc: "https://drive.google.com/file/d/1R1UVPthRhr4Og-as6FTUoKHgC3u83iX4/view?usp=sharing",
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img:"https://upload.wikimedia.org/wikipedia/en/b/b0/Indian_Institute_of_Technology_%28Indian_School_of_Mines%29%2C_Dhanbad_Logo.png",
+      school: "Indian Institute of Technology,Dhanbad",
+      date: "December 2021 - May 2025",
+      grade: "8.21 CGPA",
+      desc: "I am currently pursuing a Bachelor's degree in Electrical Engineering at IIT,Dhanbad, where I am learning and working on exciting projects with a team of talented developers.",
+      degree: "Bachelor of Technology - BTech, Electrical Engineering",
+    },
+    {
+      id: 1,
+      img: "https://rajagiri.ac.in/wp-content/uploads/2022/01/logo.png",
+      school: "RPS, Kalamassery",
+      date: "Apr 2018 - Apr 2020",
+      grade: "96.0%",
+      desc: "I completed my class 12 high school education at RPS,Kalamassery, where I studied Science along with Computer .",
+      degree: "AISSCE(XII), Science with Computer",
+    },
+    {
+      id: 2,
+      img: "https://rajagiri.ac.in/wp-content/uploads/2022/01/logo.png",
+      school: "RPS, Kalamassery",
+      date: "Apr 2017 - Apr 2019",
+      grade: "94.2%",
+      desc: "I completed my class 10 education at RPS, Kalamassery.",
+      degree: "AISSE(X), Science with Computer",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 11,
+      title: "Diabetes prediction",
+      date: "Jan 2024 ",
+      description:
+        "Built a diabetes detection model using Support Vector Machine (SVM) classifier with linear kernel .",
+      image:
+        "https://github.com/rishavchanda/DecisionHub/raw/master/assets/testRule.jpg",
+      tags: [
+        "Python",
+      ],
+      category: "ML model",
+      github: "https://github.com/jofra88/Diabetes-Prediction",
+    },
+  ];
+  
+  export const TimeLineData = [
+    { year: 2017, text: "Started my journey" },
+    { year: 2018, text: "Worked as a freelance developer" },
+    { year: 2019, text: "Founded JavaScript Mastery" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2021, text: "Started my own platform" },
+  ];
